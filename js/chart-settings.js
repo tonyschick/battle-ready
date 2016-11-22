5ac2e2d8f4d70d62d79aca04b0ba93c201f74e60
Tony Schick <schick.anthony@gmail.com>
2016-11-22 13:28:02 -0800

    style tweaks

--- js/chart-settings.js
+++ js/chart-settings.js
@@ -1,81 +1,16 @@
-
 /******** FOR LIGHT BACKGROUNDS ***********/
-// $(function() {
-//     Highcharts.setOptions({
-//         chart: {
-//           style: {
-//             fontFamily: 'Helvetica Neue',
-//           }
-//         },
-//         title: {
-//           style: {
-//             fontFamily: 'Crimson',
-//           }
-//         }
-//         lang: {
-//             thousandsSep: ','
-//         },
-//         exporting: {
-//         	enabled: false
-//         },
-//         credits: {
-//         	enabled: false
-//         },
-//         legend: {
-//           enabled: false
-//         },
-//         tooltip:  {
-//           borderWidth: 0,
-//         }
-//     });
-// });
-
-/********* FOR DARK BACKGROUNDS ***********/
-
 $(function() {
     Highcharts.setOptions({
         chart: {
-          backgroundColor: '#333',
-          // polar: true,
           style: {
             fontFamily: 'Helvetica Neue',
-            color: '#fff',
-          },
-
+          }
         },
         title: {
           style: {
-            color: '#fff',
-            fontFamily: 'Crimson Text'
+            fontFamily: 'Crimson Text',
           }
-
         },
-        yAxis: {
-          labels: {
-           style: {
-              color: '#ccc',
-           }
-        },
-
-        },
-        xAxis: {
-          labels: {
-           style: {
-              color: '#ccc',
-           }
-        },
-
-        },
-        // style: {
-        //  color: '#F00',
-        //  backgroundColor: '#333'
-        //  font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
-        // }
-        // title: {
-        //   style: {
-        //     fontFamily: 'Crimson',
-        //   }
-        // }
         lang: {
             thousandsSep: ','
         },
@@ -93,3 +28,55 @@ $(function() {
         }
     });
 });
+
+/********* FOR DARK BACKGROUNDS ***********/
+// $(function() {
+//     Highcharts.setOptions({
+//         chart: {
+//           backgroundColor: '#333',
+//           // polar: true,
+//           style: {
+//             fontFamily: 'Helvetica Neue',
+//             color: '#fff',
+//           },
+//
+//         },
+//         title: {
+//           style: {
+//             color: '#fff',
+//             fontFamily: 'Crimson Text'
+//           }
+//
+//         },
+//         yAxis: {
+//           labels: {
+//            style: {
+//               color: '#ccc',
+//            }
+//         },
+//
+//         },
+//         xAxis: {
+//           labels: {
+//            style: {
+//               color: '#ccc',
+//            }
+//          }
+//         },
+//         lang: {
+//             thousandsSep: ','
+//         },
+//         exporting: {
+//         	enabled: false
+//         },
+//         credits: {
+//         	enabled: false
+//         },
+//         legend: {
+//           enabled: false
+//         },
+//         tooltip:  {
+//           borderWidth: 0,
+//         }
+//     });
+// });
